d560614e86f7ca600bccd9ef14d70eac186b6041
islamshu12 <islamshu12@gmail.com>
2023-06-17 13:32:16 +0300

    edit

--- resources/views/close.blade.php
+++ resources/views/close.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{{ $data->website_title }}</title>
-    <link rel="stylesheet" href="{{ asset('front/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('waiting-list/front/style.css') }}" />
   </head>
   <body>
     <section class="page2" style="    background-image: url({{ $data->background_closed }});">
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/waiting-list', [HomeController::class,'fetchData'])->name('index');
-Route::get('form', [HomeController::class,'send_form'])->name('send_form');
-Route::post('send_form', [HomeController::class,'send_form_post'])->name('send_form_post');
+Route::get('/waiting-list/form', [HomeController::class,'send_form'])->name('send_form');
+Route::post('/waiting-list/send_form', [HomeController::class,'send_form_post'])->name('send_form_post');
