fb56b853aec50d12ff53cbfac2d5a6671c434965
islamshu12 <islamshu12@gmail.com>
2023-06-17 12:33:35 +0300

    edit

--- resources/views/send_form.blade.php
+++ resources/views/send_form.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{{ $data->website_title }}</title>
-    <link rel="stylesheet" href="{{ asset('front/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('waiting-list/front/style.css') }}" />
 </head>
 
 <body>
