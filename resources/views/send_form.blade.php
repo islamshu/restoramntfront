02dd39e3708ee4b148f0bb65abca575f465589ec
islamshu12 <islamshu12@gmail.com>
2023-06-18 15:52:23 +0300

    edit

--- resources/views/send_form.blade.php
+++ resources/views/send_form.blade.php
@@ -106,6 +106,7 @@
                             console.log('Order status updated:', response.status);
 
                             if (response.status ==1 ) {
+                                clearInterval(intervalId); // Stop sending further requests
                                 showNotificationPopup('Order Accepted');
                             }
                         },
