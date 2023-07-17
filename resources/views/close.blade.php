cd192b911247ca5d32122084ce5cef390d1a7579
islamshu12 <islamshu12@gmail.com>
2023-07-17 12:17:41 +0300

    edit

--- resources/views/close.blade.php
+++ resources/views/close.blade.php
@@ -16,7 +16,8 @@
       </div>
       <div class="content">
         <p class="sub_title">
-        {!! $data->close_message !!}  <br> <span>من {{ $data->start_at_new }} <br>  الى {{ $data->end_at_new }} </span> <br> 
+        {!! $data->close_message !!}  <br> <span> {{ $data->start_at_new }} <br>  الى {{ $data->end_at_new }} </span> <br> 
+        {{ $data->start_at_new }} من
         {!! $data->close_message_en !!}  form {{ $data->start_at_new }} to {{ $data->end_at_new }}<br> 
 
 
