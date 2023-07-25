eab4c655570b8c13117f107f7761f3c4b4a4f833
islamshu12 <islamshu12@gmail.com>
2023-07-25 11:07:08 +0300

    edit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,23 +29,22 @@ class HomeController extends Controller
 
         $data = $response->getBody()->getContents();
         $dataa =  json_decode($data)->data;
-        if($dataa->is_open == 0){
-            return view('manual_close')->with('data',$dataa);
-
+        if ($dataa->is_open == 0) {
+            return view('manual_close')->with('data', $dataa);
         }
-        if($dataa->now_queue > $dataa->max_order){
-            return view('queue_close')->with('data',$dataa);
+        if ($dataa->now_queue > $dataa->max_order) {
+            return view('queue_close')->with('data', $dataa);
         }
-        if($dataa->is_manual_close == 0){
-            return view('close')->with('data',$dataa);
-
+        if ($dataa->is_manual_close == 0) {
+            return view('close')->with('data', $dataa);
         }
-       
-        
 
-        return view('send_form')->with('data',$dataa);
+
+
+        return view('send_form')->with('data', $dataa);
     }
-    public function send_form_post(Request $request){
+    public function send_form_post(Request $request)
+    {
 
         $client = new Client();
         $response = $client->get('https://dashboard.primecut.me/api/all_data');
@@ -53,12 +52,13 @@ class HomeController extends Controller
 
         $data = $response->getBody()->getContents();
         $dataa =  json_decode($data)->data;
-        $dataa->now_queue > $dataa->max_order;
-        return response()->json(['status' => 'fail','message'=>'تم الوصول الى الحد الاقصى يرجى المعاودة لاحقا']);
 
+        if ($dataa->now_queue > $dataa->max_order) {
+            return response()->json(['status' => 'fail', 'message' => 'تم الوصول الى الحد الاقصى يرجى المعاودة لاحقا']);
+        }
         $response = Http::post('https://dashboard.primecut.me/api/make_order', [
 
-        // $response = Http::post('http://127.0.0.1:8000/api/make_order', [
+            // $response = Http::post('http://127.0.0.1:8000/api/make_order', [
             'name' => $request->name,
             'phone' => $request->phone,
             'guest' => $request->guest,
@@ -67,33 +67,27 @@ class HomeController extends Controller
             'place_type' => $request->table_type,
         ]);
         $responseData = $response->json();
-        if($responseData['success'] == true){
-            return response()->json(['status' => 'success','orderId'=>$responseData['data']['code']]);
-        }else{
-            return response()->json(['status' => 'fail','message'=>'حدث خطأ ما يرجى المحاولة لاحقا']);
-
+        if ($responseData['success'] == true) {
+            return response()->json(['status' => 'success', 'orderId' => $responseData['data']['code']]);
+        }elseif($responseData['success'] == 'error') {
+            return response()->json(['status' => 'fail', 'message' =>  $responseData['data']['message']]);
+        } else  {
+            return response()->json(['status' => 'fail', 'message' => 'حدث خطأ ما يرجى المحاولة لاحقا']);
         }
-        
-
-
     }
-    public function resend_request(Request $request){
+    public function resend_request(Request $request)
+    {
         dd($request);
         $response = Http::post('https://dashboard.primecut.me/api/resend_request', [
             'order_id' => $request->order_id,
         ]);
         dd($response);
-        
+
         $responseData = $response->json();
-        if($responseData['success'] == true){
-            return response()->json(['status' => 'success','orderId'=>$responseData['data']['code']]);
-        }else{
+        if ($responseData['success'] == true) {
+            return response()->json(['status' => 'success', 'orderId' => $responseData['data']['code']]);
+        } else {
             return response()->json(['status' => 'fail']);
         }
-        
-
-
     }
-
-    
 }
