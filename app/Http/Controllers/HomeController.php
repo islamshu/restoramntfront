bceae3a2dd0e7841ce066dc6b977bfddf94e4cd3
islamshu12 <islamshu12@gmail.com>
2023-07-25 11:13:15 +0300

    edit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -67,6 +67,7 @@ class HomeController extends Controller
             'place_type' => $request->table_type,
         ]);
         $responseData = $response->json();
+        return  $responseData;
         if ($responseData['success'] == true) {
             return response()->json(['status' => 'success', 'orderId' => $responseData['data']['code']]);
         }elseif($responseData['success'] == 'error') {
